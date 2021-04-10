ff551f50b43cce9b870b09fc9e7a0d35e5cac9b0
hideakiarai <1988.ja8n@gmail.com>
2021-04-10 05:30:23 +0000

    課題完了

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,22 @@
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::post('news/create', 'Admin\NewsController@create');
 });
 
 Route::get('admin/profile/create', 'Admin\ProfileController@add')->middleware('auth');
 
+Route::post('admin/profile/create', 'Admin\ProfileController@create')->middleware('auth');
+
 Route::get('admin/profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 
+Route::post('admin/profile/edit', 'Admin\ProfileController@update')->middleware('auth');
 
 Auth::routes();
 
